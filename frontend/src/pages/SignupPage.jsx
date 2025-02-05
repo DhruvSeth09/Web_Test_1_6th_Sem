b72a2b02dea9971f415364e7a709d12172a05460
Dhruv Seth <122626186+DhruvSeth09@users.noreply.github.com>
2025-02-05 16:32:17 +0530

    project upload

---
+++ frontend/src/pages/SignupPage.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header"
+import Signup from "../components/Signup"
+export default function LoginPage() {
+    
+
+    return (
+        <>
+            <Header/>
+            <Signup/>
+        </>
+    )
+}
